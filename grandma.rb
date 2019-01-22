7653e92aa6808a700f3f563a93dbf8a2d76a8319
Christoph Gruener <christophgruener@me.com>
2019-01-22 17:36:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,5 +19,5 @@ def speak_to_grandma(phrase)
   elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!
+    return "NO, NOT SINCE 1938!"
 end
